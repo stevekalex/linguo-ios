c41e6dbe529be55a2c64d3cea44782a15af0def7
Steve A <stevekalex@gmail.com>
2024-08-13 14:33:54 +0100

    feat: create LoginView template

--- Linguo/Core/Authentication/LoginView.swift
+++ Linguo/Core/Authentication/LoginView.swift
@@ -3,22 +3,85 @@
 //  Linguo
 //
 //  Created by Steve Alex on 12/08/2024.
-//
 
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
         NavigationStack {
             VStack {
-                Image("linguo-logo")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 150, height: 150)
-                    .padding(.vertical, 32)
+                VStack {
+                    HStack (alignment: .bottom, content: {
+                        Text("Welcome to")
+                            .font(.custom("Avenir Next Bold", size: 25))
+                            .frame(alignment: .bottomLeading)
+                            .padding(.bottom, 10)
+                        
+                        Image("linguo-logo")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .padding(.bottom, -27)
+                        
+                            
+                    })
+                    .padding(.top, 100)
+                    
+                    Text("Linguo")
+                        .font(.custom("Avenir Next Bold", size: 79))
+                        .padding(.top, -20)
+                        .foregroundColor(Color(.systemBlue))
+                }
+
+                
+                VStack(spacing: 18) {
+                    InputView(text: $email,
+                              title: "Email Address",
+                              placeholder: "timothy@chalamet.com")
+                    .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                    
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "************")
+                }
+                .padding([.leading, .trailing], 80)
+                
+                Button (action: {}) {
+                    Text("SIGN IN")
+                        .font(.custom("Avenir Next Bold", size: 18))
+                        .fontWeight(.semibold)
+                        .frame(width: UIScreen.main.bounds.width - 172, height: 48)
+                        .foregroundColor(.white)
+                        .background(LinearGradient(gradient: Gradient(colors: [.teal, .blue]), startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(40)
+                        .padding(.top, 30)
+                }
+                NavigationLink {} label: {
+                    Text("Forgot password")
+                        .foregroundColor(.black)
+                        .font(.custom("Avenir Next", size: 14))
+                        .padding(.top, 15)
+                }
+                
+                
+                Spacer()
+                
+                NavigationLink {} label: {
+                    HStack(spacing: 2) {
+                        Text("Don't have an account?")
+                            .foregroundColor(.black)
+                        
+                        Text("Sign up")
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(Color(.systemBlue))
+                            
+                    }
+                    .font(.custom("Avenir Next", size: 14))
+                }
+                .padding(.bottom, 40)
             }
-            
-            Spacer()
         }
     }
     
---
+++ Linguo/Core/Authentication/RegistrationView.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationView.swift
+//  Linguo
+//
+//  Created by Steve Alex on 13/08/2024.
+//
+
+import Foundation
