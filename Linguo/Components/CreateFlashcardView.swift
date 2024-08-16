ce8453c271e1e3ee0c54df5a18d280ca5416f3c1
Steve A <stevekalex@gmail.com>
2024-08-16 11:21:12 +0100

    feat: Create add and review new flashcard buttons along with view for both

---
+++ Linguo/Components/CreateFlashcardView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateFlashcardView.swift
+//  Linguo
+//
+//  Created by Steve Alex on 16/08/2024.
+
+import Foundation
+import SwiftUI
+
+struct CreateFlashcardView: View {
+    var body: some View {
+        Text("Flashcard view")
+    }
+}
+
+struct CreateFlashcardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateFlashcardView()
+    }
+}
---
+++ Linguo/Components/ReviewFlashcardView.swift
@@ -0,0 +1,21 @@
+//
+//  ReviewFlashcardView.swift
+//  Linguo
+//
+//  Created by Steve Alex on 16/08/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct ReviewFlashcardView: View {
+    var body: some View {
+        Text("Review flashcard view")
+    }
+}
+
+struct ReviewFlashcardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateFlashcardView()
+    }
+}
