16415bf0b15657bd23f35bee45ca089b40c042b9
Steve A <stevekalex@gmail.com>
2024-08-31 09:28:13 +0100

    feat: scaffold ReviewFlashcardView View

--- Linguo/Components/ReviewFlashcardView.swift
+++ Linguo/Components/ReviewFlashcardView.swift
@@ -13,11 +13,16 @@ struct ReviewFlashcardView: View {
     @State var deckId: String
     @StateObject private var flashcardServce = FlashcardService()
     @State private var flashcards: [FlashcardData] = []
+    @State private var flashcard: FlashcardData?;
+    @State private var showFlashcardView = false
+    @State private var currentIndex: Int = 0
     
     func getFlashCardData(deckId: String, reviewDate: Date) async {
         do {
-            let fetchedFlashcards = try await flashcardServce.getFlashcards(deckId: deckId, reviewDate: reviewDate)
-            self.flashcards = fetchedFlashcards // Update the state variable
+            let fetchedFlashcards = try await flashcardServce.getFlashcards(deckId: deckId, reviewDate: reviewDate);
+            self.flashcards = fetchedFlashcards
+            self.flashcard = fetchedFlashcards[0]
+            self.showFlashcardView = true // Set showFlashcardView to true after retrieving flashcards
         } catch {
             print("Error loading flashcards")
         }
@@ -25,14 +30,27 @@ struct ReviewFlashcardView: View {
     
     var body: some View {
         VStack {
-            Text("deckId \(deckId)")
-            Text("Review flashcard view")
-            ForEach(flashcards) { flashcard in
-                Image(uiImage: flashcard.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-                Text(flashcard.answer)
+            if showFlashcardView {
+                VStack {
+                    if (flashcard!.promptImage != nil) {
+                        Image(uiImage: flashcard!.promptImage!)
+                            .resizable()
+                            .scaledToFit()
+
+                    } else  {
+                        Text(flashcard!.promptString!)
+                    }
+                }
+                .onTapGesture {
+                    print("Tapped")
+                    flashcards.remove(at: 0)
+                    
+                    if ($flashcards.isEmpty) {
+                        showFlashcardView = false
+                    } else {
+                        flashcard = flashcards[0]
+                    }
+                }
             }
         }
         .onAppear {
@@ -43,8 +61,8 @@ struct ReviewFlashcardView: View {
     }
 }
 
-struct ReviewFlashcardView_Previews: PreviewProvider {
-    static var previews: some View {
-        ReviewFlashcardView(deckId: "123")
-    }
-}
+//struct ReviewFlashcardView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ReviewFlashcardView(deckId: "123")
+//    }
+//}
