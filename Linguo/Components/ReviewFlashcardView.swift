989be02e2046b7e361b59f0dd71d55da5fb824b2
Steve A <stevekalex@gmail.com>
2024-08-31 10:39:55 +0100

    feat: abstract flashcard logic into FlashcardView, enable reveal answer on tap, and buttons for resetting state

--- Linguo/Components/ReviewFlashcardView.swift
+++ Linguo/Components/ReviewFlashcardView.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 import FirebaseStorage
 
-struct ReviewFlashcardView: View {
+struct FlashcardReviewSessionView: View {
     @State var deckId: String
     @StateObject private var flashcardServce = FlashcardService()
     @State private var flashcards: [FlashcardData] = []
@@ -22,7 +22,7 @@ struct ReviewFlashcardView: View {
             let fetchedFlashcards = try await flashcardServce.getFlashcards(deckId: deckId, reviewDate: reviewDate);
             self.flashcards = fetchedFlashcards
             self.flashcard = fetchedFlashcards[0]
-            self.showFlashcardView = true // Set showFlashcardView to true after retrieving flashcards
+            self.showFlashcardView = true
         } catch {
             print("Error loading flashcards")
         }
@@ -31,26 +31,21 @@ struct ReviewFlashcardView: View {
     var body: some View {
         VStack {
             if showFlashcardView {
-                VStack {
-                    if (flashcard!.promptImage != nil) {
-                        Image(uiImage: flashcard!.promptImage!)
-                            .resizable()
-                            .scaledToFit()
-
-                    } else  {
-                        Text(flashcard!.promptString!)
+                FlashcardView(
+                    flashcard: flashcard!,
+                    flashcards: $flashcards,
+                    showFlashcardView: $showFlashcardView
+                )
+                    .onTapGesture {
+                        print("Tapped")
+                        flashcards.remove(at: 0)
+                        
+                        if ($flashcards.isEmpty) {
+                            showFlashcardView = false
+                        } else {
+                            flashcard = flashcards[0]
+                        }
                     }
-                }
-                .onTapGesture {
-                    print("Tapped")
-                    flashcards.remove(at: 0)
-                    
-                    if ($flashcards.isEmpty) {
-                        showFlashcardView = false
-                    } else {
-                        flashcard = flashcards[0]
-                    }
-                }
             }
         }
         .onAppear {
@@ -60,9 +55,3 @@ struct ReviewFlashcardView: View {
         }
     }
 }
-
-//struct ReviewFlashcardView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ReviewFlashcardView(deckId: "123")
-//    }
-//}
